98797979a83936f14e3d327992a1d64819f0f316
Dennis de Oliveira <contato@dennisoliveira.com.br>
2018-05-03 15:07:16 -0300

    Adicionado vue router e vue resource ao main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+const router = new VueRouter({
+  linkExactActiveClass: 'active'
+})
+
 new Vue({
   el: '#app',
+  router: router,
   render: h => h(App)
 })
