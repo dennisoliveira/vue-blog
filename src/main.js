4231cbeb7345f5a6af2fa42f64b38d094cb1bf36
Dennis de Oliveira <contato@dennisoliveira.com.br>
2018-05-03 19:30:43 -0300

    Adicionado vue-validator ao projeto

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
+import VueValidator from 'vue-validator'
 
 import Routes from './routes'
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
+Vue.use(VueValidator)
 
 const router = new VueRouter({
   //linkActiveClass: 'active',
